cdf58a7aef9fa09a69c3a1a60cbb17638ee56a74
sharonding <xianghua.ding@gmail.com>
2022-03-19 21:31:01 +0000

    changed settings

--- dingdemo/dingdemo/urls.py
+++ dingdemo/dingdemo/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from dingapp import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('dingapp/', include('dingapp.urls')),
+    # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
