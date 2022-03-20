f78f735b1d29f11a719355ef21075f90a6092e71
sharonding <xianghua.ding@gmail.com>
2022-03-20 10:18:12 +0000

    databasedone

---
+++ dingdemo/datapopulation.py
@@ -0,0 +1,68 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+     'dingdemo.settings')
+
+import datetime
+import django
+
+django.setup()
+
+from dingapp.models import Course, Student
+
+def populate():
+    # First, we will create lists of students containing the pages
+    # we want to add into each course.
+    # Then we will create a dictionary of dictionaries for our courses.
+    # This might seem a little bit confusing, but it allows us to iterate
+    # through each data structure, and add the data to our models.
+
+    was_students = [
+    {'name': 'Aiden',
+    'dob': datetime.date(2000, 7, 3),
+    'url':'http://www.google.com'},
+    {'name':'Ashley',
+    'dob': datetime.date(2002, 1, 3),
+        'url':'http://www.google.com'},
+    ]
+
+    se_students = [
+    {'name': 'Finley',
+    'dob': datetime.date(1999, 8, 3),
+        'url':'http://www.google.com'},
+    {'name':'James',
+    'dob': datetime.date(2001, 3, 10),
+     'url':'http://www.google.com'},
+    ]
+
+
+    courses = {'Web Application Systems': {'students': was_students},
+    'Software Engineering': {'students': se_students},}
+
+
+
+    # If you want to add more courses or students,
+    # add them to the dictionaries above.
+
+    # The code below goes through the course dictionary, then adds each course,
+    # and then adds all the associated students for that course.
+    for cname, cdata in courses.items():
+        c = add_course(cname)
+        for s in cdata['students']:
+            add_student(c, s['name'], s['dob'],s['url'])
+
+
+def add_student(course, name, br, url):
+        s = Student.objects.get_or_create(course=course, name=name, dateofbirth=br)[0]
+        #s.dateofbirth = br
+        s.url=url
+        s.save()
+        return s
+
+def add_course(name):
+        c = Course.objects.get_or_create(name=name)[0]
+        c.save()
+        return c
+
+if __name__ == '__main__':
+    print('Starting dingapp population script...')
+    populate()
--- dingdemo/dingapp/models.py
+++ dingdemo/dingapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        return self.name
+
+class Student(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128)
+    url = models.URLField()
+    dateofbirth = models.DateField()
+
+    class Meta:
+        verbose_name_plural = 'Students'
+
+    def __str__(self):
+        return self.name
